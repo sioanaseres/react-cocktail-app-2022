79c818759f73c8b0a9020acb1fbffdbfbb2a6f54
Ioana Seres <ioana.cosoreanuseres@888holdings.com>
2022-07-13 12:53:53 +0300

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="Cocktail db logo" className="logo" />
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+        obcaecati voluptatibus dolore sint optio, cumque repudiandae alias
+        similique, beatae, totam consectetur itaque corrupti omnis repellat
+        exercitationem. Temporibus quidem, architecto molestias minus natus vero
+        nemo molestiae animi doloribus ratione illo sed?
+      </p>
+    </section>
+  );
+};
+
+export default About;
